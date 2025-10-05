6651bf4c239f9457222ca47e1e53c3e44a102a67
vaishnavi <moravaishnavi123@gmail.com>
2025-10-05 13:46:07 +0530

    variables

--- 04.variable.sh
+++ 04.variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "please enter your PIN number"
-read PIN_NUMBER #here PIN_NUMBER is vatiable
+read -s PIN_NUMBER #here PIN_NUMBER is vatiable
 
 echo "PIN number is: $PIN_NUMBER"
