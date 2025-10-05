e3dd0744ace494b9488a293f0bbea9d21430d3dc
vaishnavi <moravaishnavi123@gmail.com>
2025-10-05 13:34:14 +0530

    variables

---
+++ 03.variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1:hi Mr.$person2"
+echo "$person2:hello Mr.$person1!!"
+echo "$person1: how are you ?"
+echo "$person2: I'm GREAT"
+echo "$person1: welcome $person2"
+echo "$person2: thanks you $person1"
