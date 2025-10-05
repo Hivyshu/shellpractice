9b35567f34ee9570c20f01e86e62f88ee473659b
vaishnavi <moravaishnavi123@gmail.com>
2025-10-05 14:34:04 +0530

    variables

--- 06.variable.sh
+++ 06.variable.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-DATE=$(date)
-
-echo "timestamp execute: $DATE"
+#DATE=$(date)
+START_TIME=$(date +%s)
+sleep 10 &
+END_TIME=$(date +%s)
+TOTAL_TIME=$($END_TIME-$START_TIME)
+echo "script executed in:$TOTAL_TIME seconds"
