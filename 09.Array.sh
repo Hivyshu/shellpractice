1abe4b58cbe951fc16c245f4c168362504f61a98
vaishnavi <moravaishnavi123@gmail.com>
2025-10-05 15:26:10 +0530

    variables

--- 09.Array.sh
+++ 09.Array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LEADERS=("Modi","Putin","Trump")
+LEADERS=("Modi" "Putin" "Trump")
 
 echo "All Leaders: ${LEADERS[@]}"
 echo "1st Leader is: ${LEADERS[0]}"
 echo "Last leader is: ${LEADERS[2]}"
-echo "BEST LEADERS ARE:${LEADERS[1][3]}"
+echo "BEST LEADERS ARE:${LEADERS[1],[3]}"
