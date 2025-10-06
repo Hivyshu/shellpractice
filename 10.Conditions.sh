a804e1b5dc4d77f034b9f6705258b4399d362f97
vaishnavi <moravaishnavi123@gmail.com>
2025-10-06 14:42:42 +0530

    variables

--- 10.Conditions.sh
+++ 10.Conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 if [ $NUMBER -lt 10 ];then
    echo "given number $NUMBER is less then 10"
 elif [ $NUMBER -eq 10 ];then 
-   ech  "given number $NUMBER is equal to 10"
+   echo  "given number $NUMBER is equal to 10"
 else      
    echo "given number $NUMBER is greater then 10"
 fi
