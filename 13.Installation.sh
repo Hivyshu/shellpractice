9d0eca29df8b7816d9388e86cd83b7bc65a84357
vaishnavi <moravaishnavi123@gmail.com>
2025-10-06 16:39:03 +0530

    variables

--- 13.Installation.sh
+++ 13.Installation.sh
@@ -13,6 +13,25 @@ if [ $? -ne 0 ];then
  echo "ERROR:: Installing mysql is failure"
  exit 1
 else 
- echo "Installing my sql is SUCCESS!!!"
+ echo "Installing mysql is SUCCESS!!!"
+
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ];then
+ echo "ERROR:: Installing nginx is failure"
+ exit 1
+else 
+ echo "Installing nginx is SUCCESS!!!"
+
+fi
+ dnf install mongodb -y
+
+if [ $? -ne 0 ];then
+ echo "ERROR:: Installing mongodb is failure"
+ exit 1
+else 
+ echo "Installing mongodb is SUCCESS!!!"
 
 fi
