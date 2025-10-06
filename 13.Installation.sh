88b13cce1117c3360dc015ebac514cc3a38bc6db
vaishnavi <moravaishnavi123@gmail.com>
2025-10-06 16:26:11 +0530

    variables

---
+++ 13.Installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+UDERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+ echo "ERROR:: Please run this with root user"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ];then
+ echo "ERROR:: Installing mysql is failure"
+
+else 
+ echo "Installing my sql is SUCCESS!!!"
+
+fi
